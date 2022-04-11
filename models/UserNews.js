02bb2445bef102f756048440319313463c9bfd06
Tomáš Zifčák <t.zifcak@seznam.cz>
2022-04-11 21:35:43 +0200

    initial commit

---
+++ models/UserNews.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema, Types } = mongoose;
+
+const userNewsSchema = new Schema(
+  {
+    newsDescription: String,
+    createdDateNews: Date,
+    authorNews: String,
+    titleNews: String,
+    userId: {
+      type: Types.ObjectId,
+      ref: "users",
+      index: true,
+    },
+  },
+  { versionKey: false }
+);
+
+mongoose.model("user_news", userNewsSchema);
---
+++ routes/uploadFileRoutes.js
@@ -0,0 +1,137 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const uploadFileSchema = mongoose.model("user_images");
+const fs = require("fs");
+const path = require("path");
+const formidable = require("formidable");
+
+const TOKEN_SECRET = "icoders_secret";
+
+const verifyToken = (token) => {
+  return jwt.verify(token, TOKEN_SECRET);
+};
+
+module.exports = (app) => {
+  // endpoint for get images for user by id
+  app.get("/api/user/image/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const userImage = await uploadFileSchema.findOne({ _id: id });
+      const rawData = fs.readFileSync(userImage.fileUrl);
+
+      res.set("Content-Type", userImage.fileType);
+
+      return res.status(200).send(rawData);
+    } catch (e) {
+      return res.status(500).send("internal server error");
+    }
+  });
+
+  // endpoint for create user image by id
+  app.post(`/api/user/image`, async (req, res) => {
+    res.set("Content-Type", "multipart/form-data");
+
+    if (req.headers && req.headers.authorization) {
+      let authorization = req.headers.authorization,
+        decoded;
+      const token = authorization.split(" ")[1];
+      try {
+        decoded = verifyToken(token);
+
+        const form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          const oldPath = files.image.path;
+          if (!fs.existsSync(`./assets/${decoded.id}`)) {
+            fs.mkdirSync(`./assets/${decoded.id}`, {
+              recursive: true,
+            });
+          }
+          const newPath =
+            path.join("./assets", `./assets/${decoded.id}`) +
+            "/" +
+            files.image.name;
+          const rawData = fs.readFileSync(oldPath);
+          const reqBody = {
+            fileName: files.image.name,
+            fileSize: files.image.size,
+            fileType: files.image.type,
+            fileUrl: newPath,
+            id: decoded.id,
+          };
+
+          fs.writeFile(newPath, rawData, async function (err) {
+            if (err) console.log(err);
+            const uploadFile = await uploadFileSchema.create(reqBody);
+            return res.status(200).send({
+              error: false,
+              statusMessage: {
+                type: "success",
+                message: "Úspěšné nahrání obrázku",
+              },
+              uploadFile,
+            });
+          });
+        });
+      } catch (e) {
+        return res.status(401).send("unauthorized");
+      }
+    } else {
+      return res.status(500).send("internal server error");
+    }
+  });
+
+  // endpoint for update user image status with admin
+  app.put(`/api/user/image`, async (req, res) => {
+    res.set("Content-Type", "multipart/form-data");
+
+    if (req.headers && req.headers.authorization) {
+      let authorization = req.headers.authorization,
+        decoded;
+      const token = authorization.split(" ")[1];
+      try {
+        decoded = verifyToken(token);
+
+        const form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          const oldPath = files.image.path;
+          if (!fs.existsSync(`./assets/${decoded.id}`)) {
+            fs.mkdirSync(`./assets/${decoded.id}`, {
+              recursive: true,
+            });
+          }
+          const newPath =
+            path.join("./assets", `./assets/${decoded.id}`) +
+            "/" +
+            files.image.name;
+          const rawData = fs.readFileSync(oldPath);
+          const reqBody = {
+            fileName: files.image.name,
+            fileSize: files.image.size,
+            fileType: files.image.type,
+            fileUrl: newPath,
+            id: decoded.id,
+          };
+
+          fs.writeFile(newPath, rawData, async function (err) {
+            if (err) console.log(err);
+            await uploadFileSchema.findOneAndDelete({ id: decoded.id });
+            const uploadFile = await uploadFileSchema.create(reqBody);
+
+            return res.status(200).send({
+              error: false,
+              statusMessage: {
+                type: "success",
+                message: "Úspěšné nahrání obrázku",
+              },
+              uploadFile,
+            });
+          });
+        });
+      } catch (e) {
+        return res.status(401).send("unauthorized");
+      }
+    } else {
+      return res.status(500).send("internal server error");
+    }
+  });
+};
